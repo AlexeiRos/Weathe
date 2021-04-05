b2149e760f44813cb6bdd8e23e6f16578b7e8a14
AlexeiRos <rostokin.s@mail.ru>
2021-04-05 14:29:01 +0300

    upload

--- WeatherApp/settings.py
+++ WeatherApp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'weather',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'))]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+django_heroku.settings(locals())
